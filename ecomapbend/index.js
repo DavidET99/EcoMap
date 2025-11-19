568739b988d5351c7008dbd10413f06226b9b3f6
DavidET99 <d.escobart@duocuc.cl>
2025-11-19 00:07:57 -0300

    arreglo index

--- ecomapbend/index.js
+++ ecomapbend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://tu-app.netlify.app',
+    'https://https://ecomapmobile.netlify.app/',
     'https://*.netlify.app'
   ],
   credentials: true
